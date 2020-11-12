212e258ce0c0661c0956f68e7b6d83f655b92282
Raj <rajkamal11126@gmail.com>
2020-11-13 01:01:43 +0530

    Refactor : Functionality to add max working hrs

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,6 +1,6 @@
 const Utility = require('./Utility');
 const totalSalary = Utility.computeEmpWage();;
-console.log(`Total salary = ${totalSalary}`);
+console.log(`Total salary = ${totalSalary}`); //To get Total salary of employee
 
 
 
--- Utility.js
+++ Utility.js
@@ -1,7 +1,11 @@
 module.exports = { computeEmpWage }
 
+// constant variables
 const empRatePerHrs = 20;
 const daysPerMonth = 20;
+const maxWorkingHrs = 100;
+
+// To get employee wage per month
 function getWorkHrs() {
     let randomValue = Math.floor(Math.random() * 10) % 3;
     for (let i = 0; i < daysPerMonth; i++) {
@@ -16,12 +20,18 @@ function getWorkHrs() {
                 empHrs = 0;
                 return empRatePerHrs * empHrs;
         }
+
     }
 }
+
+//To compute total employee wage 
 function computeEmpWage() {
+
+    // local variables
     let totalWage = 0,
         dayCount = 1;
-    while (dayCount <= daysPerMonth) {
+    let totalWorkingHrs = 0;
+    while (dayCount <= daysPerMonth || totalWorkingHrs == maxWorkingHrs) {
         totalWage += empRatePerHrs * getWorkHrs();
         dayCount++;
     }
